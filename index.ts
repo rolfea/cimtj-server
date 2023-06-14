fc75ce7f48e9ed2a41cdaa070aade2a3e24bf9db
Alex Rolfe <alex.rolfe27@gmail.com>
2023-06-14 08:41:06 -0500

    First commit

---
+++ index.ts
@@ -0,0 +1,34 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', async (req: Request, res: Response) => {
+  const jokeText = req.query.jokeText;
+  const url =
+    process.env.PERSPECTIVE_API + `?key=${process.env.PERSPECTIVE_KEY}`;
+
+  const results = await (
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        comment: { text: jokeText },
+        languages: ['en'],
+        requestedAttributes: { TOXICITY: {} },
+      }),
+    })
+  ).json();
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.send(results);
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
